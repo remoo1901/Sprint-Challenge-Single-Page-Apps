8c78e89c59809cd2c26f8dd5f8845bfdd8771f0c
remoo1901 <remoo1901@hotmail.com>
2020-01-24 20:07:41 -0500

    still working on MVP

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [char, setChar] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+   
+      axios
+        .get(`https://rickandmortyapi.com/api/character/`)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    
   }, []);
 
   return (
