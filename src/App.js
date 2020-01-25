e640bafe895bdfc74f649f8db4728825431c3d06
remoo1901 <remoo1901@hotmail.com>
2020-01-24 21:32:33 -0500

    finished home page and characters page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
-
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <Link className="" exact to="/">
+        Home
+      </Link>
+      <Link className="" exact to="/Character">
+        Characters 
+      </Link>
+      <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/Character" component={CharacterList} />
     </main>
   );
 }
